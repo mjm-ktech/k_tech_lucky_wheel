42649f03142d9e1eccb833996cd8e80d96eb25d2
minhle <duyminh2032000@gmail.com>
2024-06-11 14:37:55 +0700

    feat: lucky wheel api and export data

---
+++ config/excel.ts
@@ -0,0 +1,13 @@
+export default {
+  config: {
+    "api::customer.customer": {
+      columns: [
+        "email",
+        "fullname",
+        "phone",
+        "prize"
+      ],
+      relation: {},
+    },
+  },
+};
